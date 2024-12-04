eebb1780884b7c6fef765a3ec21e0db6a979a517
Sarathsurendhran <sarathsurendhran15@gmail.com>
2024-12-04 16:51:11 +0530

    email host changed

--- sonichub/sonichub/settings.py
+++ sonichub/sonichub/settings.py
@@ -151,8 +151,8 @@ MESSAGE_TAGS={
 # Email...
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS= config('EMAIL_USE_TLS', cast=bool)
-EMAIL_HOST='smtp.office365.com'
+EMAIL_USE_TLS = True
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER= config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT=587
